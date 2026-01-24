e2b22c901a7a35a7e25ec0665b6b6fbe61f60c0e
SunnyPmc <102278349+SunnyPmc@users.noreply.github.com>
2026-01-24 21:02:57 +0545

    Update API URL for production

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Loading from "../components/common/loading/Loading";
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
-// const SingleBlogPage = import("../pages/singleBlogPage")
+
 const singleBlogPage = lazy(() => import("../pages/singleBlogPage"));
 
 // const repoName = import.meta.env.VITE_REPO_NAME || "";
@@ -26,7 +26,7 @@ export const router = createBrowserRouter(
           path: "/blogs/:id",
           element: (
             <Suspense fallback={<Loading />}>
-              <SingleBlogPage />
+              <singleBlogPage />
             </Suspense>
           ),
           // errorElement: <ErrorPage />
